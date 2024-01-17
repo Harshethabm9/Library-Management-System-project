793268a631f06abd4772283dd3e7fd20b452f584
harshethabm9 <harshethabm@gmail.com>
2024-01-17 20:02:21 +0530

    Library Class added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Student():
+class Student:
     def requestBook(self):
         print("So, you want to borrow book!")
         self.book = input("Enter name of the book you want to borrow: ")
@@ -16,4 +16,31 @@ class Student():
         print("\nOkay! You want to doante book!")
         self.book = input("\nEnter the name of the book you want to donate: ")
         return self.book
+
+class Library:
+    def __init__(self, listofBooks):
+        self.books = listofBooks
+
+    def displayAvailableBooks(self):
+        print(f"\n{len(self.books)} AVAILABLE BOOKS ARE: ")
+        for book in self.books:
+            print(" ♦-- " + book)
+        print("\n")
+
+    def borrowBook(self, name, bookname):
+        if bookname not in self.books:
+            print(
+                f"{bookname} BOOK IS NOT AVAILABLE EITHER TAKEN BY SOMEONE ELSE, WAIT UNTIL HE RETURNED.\n")
+        else:
+            track.append({name: bookname})
+            print("BOOK ISSUED!\nTHANK YOU KEEP IT WITH CARE AND RETURN ON TIME.\n")
+            self.books.remove(bookname)
+
+    def returnBook(self, bookname):
+        print("BOOK RETURNED!\n THANK YOU! \n")
+        self.books.append(bookname)
+
+    def donateBook(self, bookname):
+        print("BOOK DONATED! \nTHANK YOU VERY MUCH, HAVE A GREAT DAY AHEAD.\n")
+        self.books.append(bookname)
 
